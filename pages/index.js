04669cb7a67375a45ae90796e220b442e19ae6a3
mattmaneval <manevalmusic@gmail.com>
2021-09-09 16:57:13 -0400

    Favicon / Meta Tags

--- pages/index.js
+++ pages/index.js
@@ -24,12 +24,12 @@ export default function Home() {
         <meta property="og:url" content="https://metatags.io/" />
         <meta property="og:title" content="Louie's Pizza Shop — Fayette, Ohio" />
         <meta property="og:description" content="Pizza, Subs, Wings, Calzones, Burgers, and Ice Cream. " />
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="og:image" content="/public/images/meta-tag-image.png" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://metatags.io/" />
         <meta property="twitter:title" content="Louie's Pizza Shop — Fayette, Ohio" />
         <meta property="twitter:description" content="Pizza, Subs, Wings, Calzones, Burgers, and Ice Cream. " />
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="twitter:image" content="/public/images/meta-tag-image.png" />
       </Head>
       <GlobalStyle />
       <Header />
