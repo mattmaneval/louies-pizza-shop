d7864d37e5f92fbedbd215a81a8d3aa3ac5e936a
mattmaneval <manevalmusic@gmail.com>
2021-09-27 12:22:03 -0400

    Fix mapping issue and mobile tweaks

--- data/sides-local.js
+++ data/sides-local.js
@@ -2,7 +2,7 @@ export const sidesData = [
   {
     name: 'Loaded Potato Skins',
     price: {
-      sgl: '8.95',
+      oneSize: '8.95',
     },
   },
   {
@@ -15,43 +15,43 @@ export const sidesData = [
   {
     name: 'Cinnastix',
     price: {
-      sgl: '6.95',
+      oneSize: '6.95',
     },
   },
   {
     name: 'Fries',
     price: {
-      sgl: '2.00',
+      oneSize: '2.00',
     },
   },
   {
     name: 'Loaded Cheese Fries',
     price: {
-      sgl: '8.95',
+      oneSize: '8.95',
     },
   },
   {
     name: 'Macho Nacho',
     price: {
-      sgl: '10.95',
+      oneSize: '10.95',
     },
   },
   {
     name: 'Nelly\'s Nachos',
     price: {
-      sgl: '10.95',
+      oneSize: '10.95',
     },
   },
   {
     name: 'Pizza Fries',
     price: {
-      sgl: '8.95',
+      oneSize: '8.95',
     },
   },
   {
     name: 'Cheesy Garlic Toast',
     price: {
-      sgl: '4.99',
+      oneSize: '4.99',
     },
   },
 ];
