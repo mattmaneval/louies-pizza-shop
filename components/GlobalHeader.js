8e497abe5997a6c23ba35ec6b44ecb8af61ae9a4
mattmaneval <manevalmusic@gmail.com>
2022-03-23 03:26:55 -0400

    Setup sanity client

--- components/GlobalHeader.js
+++ components/GlobalHeader.js
@@ -1,29 +1,45 @@
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import sanityClient from '../client';
+import Special from './Special';
 import media from '../styles/media';
 import theme from '../styles/theme';
 
 const { color } = theme;
 
 const GlobalHeaderStyles = styled.div`
+  background-color: ${color.foregroundAlt};
+  color: ${color.background};
+  padding: 0.95em 0;
+  text-align: center;
 
-  .global-header {
-    background-color: ${color.foregroundAlt};
-    color: ${color.background};
-    padding: 0.95em 0;
-    text-align: center;
-
-    @media ${media.secondary} {
-      padding: 0.8em 0;
-    }
+  @media ${media.secondary} {
+    padding: 0.8em 0;
   }
 `;
 
-const GlobalHeader = () => (
-  <GlobalHeaderStyles>
-    <div className="global-header">
-      Special &mdash; 2 Topping Bob $25
-    </div>
-  </GlobalHeaderStyles>
-);
+export default function GlobalHeader() {
+  const [allPostsData, setAllPosts] = useState(null);
 
-export default GlobalHeader;
+  useEffect(() => {
+    sanityClient.fetch(
+      `*[_type == "specials" && (("active" in categories))] {
+        title,
+      }`,
+    )
+      .then((data) => setAllPosts(data));
+  }, []);
+
+  return (
+    <GlobalHeaderStyles>
+      {allPostsData
+      && allPostsData.map((post, index) => (
+        <Special
+          index={index}
+          special={post.title}
+        />
+      ))}
+
+    </GlobalHeaderStyles>
+  );
+}
---
+++ components/Special.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Special = ({special}) => (
+  <span>{special}</span>
+);
+
+export default Special;
